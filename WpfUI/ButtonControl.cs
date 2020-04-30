29394e092b952c06c8530152ddd5bb444839883e
Miracle <rjcvjchzljv9@gmail.com>
2020-04-30 14:02:11 +0300

    rework

---
+++ WpfUI/ButtonControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WpfUI
+{
+    class ButtonControl
+    {
+        System.Threading.Timer timer;
+        public string Button { get; set; }
+        public ButtonControl(string button)
+        {
+            Button = button;
+        }
+
+        public void Press()
+        {
+            timer = new System.Threading.Timer((state) => { SendKeys.SendWait(Button); }, null, 0, 1000);
+        }
+        public void StopPress()
+        {
+            timer.Dispose();
+        }
+
+        
+    }
+
+}
--- WpfUI/MainWindow.xaml.cs
+++ WpfUI/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace WpfUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        ButtonControl buttonControl;
+        private bool buttonIsPressed;
         public MainWindow()
         {
             InitializeComponent();
+            buttonIsPressed = false;
+            buttonStartRootines.IsEnabled = false;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonIsPressed)
+            {
+                buttonControl.StopPress();
+                buttonControl = null;
+                buttonStartRootines.Content = "start";
+                buttonIsPressed = false;
+            }
+            else
+            {
+                if (textBox.Text == "")
+                    return;
+                buttonControl = new ButtonControl(/*string.Concat('{', textBox.Text, '}')*/textBox.Text);
+                buttonControl.Press();
+                buttonStartRootines.Content = "stop";
+                buttonIsPressed = true;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            buttonStartRootines.IsEnabled = textBox.Text == "" ? false : true;
+            //if (textBox.Text == "")
+            //    buttonStartRootines.IsEnabled = false;
+            //else
+            //    buttonStartRootines.IsEnabled = true;
+        }
+
+        
+
+        private void TextBox_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            textBox.Text = "";
+        }
+
+        
     }
 }
